65c5789635016792d620b95d167dceb3c121bfc0
dj1ch <tg.1ch0712@gmail.com>
2024-05-05 16:45:21 -0700

    add new nix configuration

--- actual configs lol/configuration.nix
+++ actual configs lol/configuration.nix
@@ -8,20 +8,21 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./chrome-device.nix
     ];
-
+  
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.kernelPackages = pkgs.linuxPackages_latest;  
 
-  networking.hostName = "nixos"; # Define your hostname.
-   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-   # networking.wireless.userControlled.enable = true; 
+  networking.hostName = "blooglet"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
+  
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -45,32 +46,34 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # experimental features
+  nix.settings.experimental-features = [
+    "nix-command"
+  ];
   
-  # Enable Picom
-  # services.picom.enable = true; 
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.desktopManager.xterm.enable = false;
-
-  # Enable *insert wm here*    
-  # services.xserver.windowManager.fluxbox.enable = true;
-  # services.xserver.displayManager.defaultSession = "none+fluxbox";
-  # services.xserver.displayManager.lightdm.enable = true;
-  # services.xserver.displayManager.startx.enable = true;
-  # services.xserver.windowManager.bspwm.enable = true; 
-  # programs.hyprland.enable = true; 
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.desktopManager.plasma5.enable = true;
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
-	    xkbVariant = "";
+    xkbVariant = "";
   };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
+    };
+  };
 
   # Enable sound with pipewire.
   sound.enable = true;
@@ -90,100 +93,105 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-   services.xserver.libinput.enable = true;
+  # services.xserver.libinput.enable = true;
+  
+  # docker
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.dj1ch = {
     isNormalUser = true;
-    description = "name_goes_here";
+    description = "Isaac Hokama";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
     ];
   };
 
-  # Enable flakes? (only if needed) 
-  #nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;    
-
+  nixpkgs.config.allowUnfree = true;
+      
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    git
-    discord
-    pfetch
-    chromium
-    steam
-    spotify
-    cavalier
-    prismlauncher
-    intel-media-driver
-    # coding stuff
-    # python311
-    pcmanfm
-    # hyprland and dependancies
-    # hyprland
-    # bspwm and dependancies
-    # bspwm
-    # sxhkd 
-    # xorg.xrandr 
-    # xorg.xset    
-    # xorg.xsetroot    
-    # xorg.xwininfo
-    # xorg.xev
-    # picom
-    # nitrogen
-    # polybar
-    # rofi
-    usbimager
-    android-tools
-    # Gnome stuff 
-    # whitesur-gtk-theme
-    # apple-cursor 
-    # colloid-icon-theme
-    # whitesur-icon-theme
-    # bibata-cursors
-    # gnome.gnome-tweaks
+  #  wget
+     # internet
+     git
+     firefox
+     armcord
+     google-chrome
+
+     # cyber
+     nmap
+     wireshark
+
+     # music
+     cava
+     spotify
+
+     # system
+     wireplumber
+     btop
+     htop
+     neofetch
+     pfetch
+     intel-media-driver
+     xmrig
+
+     # rpi development
+     rpi-imager
+     vscode
+     cmake
+     pico-sdk
+     
+     # gcc
+     libgcc
+     ccacheWrapper
+     distccMasquerade
+     distccWrapper
+     gcc
+     gcc10
+     gcc11
+     gcc12
+     gcc13
+     gcc48
+     gcc49
+     gcc6
+     gcc7
+     gcc8
+     gcc9
+     gcc_debug
+     gcc_multi
+     gccgo
+     gccgo12
+     gccgo13
+     gcj
+     gdc
+     gfortran
+     gfortran10
+     gfortran11
+     gfortran12
+     gfortran13
+     gfortran48
+     gfortran49
+     gfortran6
+     gfortran7
+     gfortran8
+     gfortran9
+     gnat
+     gnat-bootstrap
+     gnat-bootstrap11
+     gnat13
+     libgccjit
+     gnumake42
+     gnumake
+     gcc-arm-embedded
+    
   ];
 
-  fonts.fonts = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk
-    noto-fonts-emoji
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-    mplus-outline-fonts.githubRelease
-    dina-font
-    proggyfonts
-    nerdfonts
-];
-
-  #enable steam 
-  hardware.opengl.driSupport32Bit = true;
-
-  environment.gnome.excludePackages = (with pkgs; [
-  gnome-photos
-  gnome-tour
-]) ++ (with pkgs.gnome; [
-  cheese # webcam tool
-  gnome-music
-  gnome-terminal
-  gedit # text editor
-  epiphany # web browser
-  geary # email reader
-  evince # document viewer
-  gnome-characters
-  totem # video player
-  tali # poker game
-  iagno # go game
-  hitori # sudoku game
-  atomix # puzzle game
-]);  
-  
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -209,6 +217,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Did you read the comment?
 
 }
